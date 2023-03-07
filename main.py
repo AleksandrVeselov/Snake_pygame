468d65f32a96138d4a24ab78239ba74fbcbe2784
Aleksandr Veselov <sanek_ves@mail.ru>
2023-03-08 00:01:02 +0300

    Откорректировано размещение игрового поля, сделаны отступы

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame
 
-SIZE = [500, 600]  # Размеры окна
 SIZE_BLOCK = 20  # Размер одного блока
 COUNT_BLOCKS = 20  # Количество блоков по горизонтали и вертикали
+HEADER_MARGIN = 70
+MARGIN = 1  # Отступ блоков друг от друга
+SIZE = [SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * SIZE_BLOCK,
+        SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * SIZE_BLOCK + HEADER_MARGIN]  # Размеры окна
 
 # Цвета игры
 FRAME_COLOR = (0, 255, 204)  # Цвет рамки
 WHITE = (255, 255, 255)
 BLUE = (204, 255, 255)
-
-MARGIN = 1  # Отступ блоков друг от друга
+HEADER_COLOR = (0, 204, 153)
 
 screen = pygame.display.set_mode(SIZE)  # создание экрана и присвоение переменной
 pygame.display.set_caption('Змейка')
@@ -20,6 +22,7 @@ while True:
             pygame.quit()
 
     screen.fill(FRAME_COLOR)  # Заливка экрана цветом
+    pygame.draw.rect(screen, HEADER_COLOR, [0, 0, SIZE[0], HEADER_MARGIN])
 
     # Отрисовка игрового поля
     for row in range(COUNT_BLOCKS):
@@ -28,7 +31,7 @@ while True:
                 color = BLUE
             else:
                 color = WHITE
-            coord_x = 10 + column * SIZE_BLOCK + MARGIN * (column + 1)  # Координата X блока
-            coord_y = 20 + row * SIZE_BLOCK + MARGIN * (row + 1)  # Координата у блока
+            coord_x = SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * (column + 1)  # Координата X блока
+            coord_y = HEADER_MARGIN + SIZE_BLOCK + row * SIZE_BLOCK + MARGIN * (row + 1)  # Координата у блока
             pygame.draw.rect(screen, color, [coord_x, coord_y, SIZE_BLOCK, SIZE_BLOCK])
     pygame.display.flip()  # Обновление экрана
